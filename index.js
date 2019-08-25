d43b6897338e5d898bbe09c5081d73796c9bfabe
Taylor <stein0209@gmail.com>
2019-08-24 19:08:51 -0600

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solutions in this file
+const welcomeMessages = [];
+
+function writeCards(names) {
+    for (let i = 0; i < names.length; i++) {
+        welcomeMessages.push(`Thank you, ${names[i]}, for the wonderful surprise gift!`);
+    }
+    return welcomeMessages;
+}
+
+function countdown(number) {
+    while (number > 0) {
+        console.log(number);
+        number -= 1;
+    }
+    console.log(number);
+}
